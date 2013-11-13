e599c1a54ebdb0b94976ffd7613ee8b1cdd87e2e
cheshun <cheshun520@gmail.com>
2013-11-13 18:57:13 +0800

    test nio

---
+++ src/main/java/org/cheshun/pushservice/server/DiscardServerHandler.java
@@ -0,0 +1,31 @@
+package org.cheshun.pushservice.server;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+
+public class DiscardServerHandler extends SimpleChannelHandler {
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+//		ChannelBuffer buf = (ChannelBuffer) e.getMessage();
+//		while(buf.readable()) {
+//			System.out.println((char) buf.readByte());
+//			System.out.flush();
+//		}
+		Channel ch = e.getChannel();
+		ch.setAttachment("hehe");
+		ch.write(e.getMessage());
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+		e.getCause().printStackTrace();
+		System.out.println("");
+		Channel ch = e.getChannel();
+		System.out.println(ch.getAttachment());
+		ch.close();
+	}
+}
